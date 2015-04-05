48b766668626e6c08c8eb04dcd95ff267f117ee9
Ryota Saito <codingsaito@gmail.com>
2015-04-04 18:10:02 -0700

    Moving Character after Goal is OK

---
+++ Assets/PlayerCharacter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : MonoBehaviour {
+
+
+	float boundX;
+	float boundY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		print ("touched goal");
+
+		Vector3 temp = transform.position;
+		temp.x += 10.0f;
+		temp.z += 10.0f;
+		transform.position = temp;
+
+	}
+}
---
+++ Assets/WinCondition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinCondition : MonoBehaviour {
+
+	//public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		//Debug.Log (target.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+			Debug.Log ("Win");
+			//transform.GetComponent<BoxCollider>().enabled = false;
+	}
+}
